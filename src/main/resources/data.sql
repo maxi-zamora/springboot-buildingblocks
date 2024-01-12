d2bed4d097352a73887a7645f5c1b903ffa05da9
maxizamora <maxizamora@USQROMAXIZAMOR1.us.deloitte.com>
2024-01-12 14:39:04 -0600

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into custom_user values(101,'a@gmail.com', 'Paulina', 'Amador', 'admin', 'ssn101', 'Pau');
+insert into custom_user values(102,'ab@gmail.com', 'Lizet', 'Rosas', 'admin', 'ssn102', 'Lizzie');
+insert into custom_user values(103,'abc@gmail.com', 'Yael', 'Pino', 'admin', 'ssn103', 'Yael');
